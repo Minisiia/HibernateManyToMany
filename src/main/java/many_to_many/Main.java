8da69555c0bd61a18063b4b694ee708e8bd50d40
Minisiia <ar_tik@yahoo.com>
2023-02-26 00:01:15 +0200

    6. JDBC & Hibernate 6. Advanced Hibernate

--- src/main/java/many_to_many/Main.java
+++ src/main/java/many_to_many/Main.java
@@ -17,30 +17,28 @@ public class Main {
     public static void main(String[] args) {
         AuthorHelper ah = new AuthorHelper();
         BookHelper bh = new BookHelper();
-
+        System.out.println((char) 27 + "[34m" + "Books with several authors:" + (char) 27 + "[38m");
         List<Book> bookList = bh.getBookList();
         for (Book book : bookList) {
             List<Author> authorList = book.getAuthors();
             if (authorList.size() > 1) {
-                LOG.info("Books with several authors:");
-                LOG.info("Book: " + book.getName());
-                LOG.info("Authors:");
+                System.out.println((char) 27 + "[34m" + "Book: " + book.getName() + (char) 27 + "[38m");
+                System.out.println((char) 27 + "[34m" + "Authors:" + (char) 27 + "[38m");
                 for (Author author : authorList) {
-                    System.out.println(author.getName() + " " + author.getLastName());
+                    LOG.info(author.getName() + " " + author.getLastName());
                 }
             }
-        }
+        } System.out.println((char) 27 + "[34m" + "Authors with several books:" + (char) 27 + "[38m");
         List<Author> authorList = ah.getAuthorList();
         Author author = new Author();
         for (int i = 1; i <= authorList.size(); i++) {
             author = ah.getAuthorById(i);
             bookList = author.getBooks();
             if (bookList.size() > 1) {
-                LOG.info("Authors with several books:");
-                LOG.info("Author: " + author.getName());
-                LOG.info("Books:");
+                System.out.println((char) 27 + "[34m" + "Author: " + author.getName() + " " + author.getLastName() + (char) 27 + "[38m");
+                System.out.println((char) 27 + "[34m" + "Books:" + (char) 27 + "[38m");
                 for (Book book : bookList) {
-                    System.out.println(book.getName());
+                    LOG.info(book.getName());
                 }
             }
 
